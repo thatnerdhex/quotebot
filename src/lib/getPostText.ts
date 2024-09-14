4e2a4a6f577a23a424da2fb44eed15711d58ce41
Hex <thatnerdhex@gmail.com>
2024-09-14 16:25:06 -0500

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,10 +1,10 @@
 export default async function getPostText() {
-  const response = await fetch("http://doomreed.info/quotes", {
+  const response = await fetch("http://doomreed.info/quotes/", {
     headers: {
       Accept: "application/json",
     },
   });
   const data = await response.json();
-  const { quotes } = data;
-  return quotes;
+  const { quotebox } = data;
+  return quotebox;
 }
